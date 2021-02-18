c4bbe769cc80c8d806f062f5f3639016149307d8
Nathan Kozyra <nkozyra@gmail.com>
2021-02-18 10:45:51 -0500

    Updates oscillators and adds icons.

--- 01_simplesynth/simplesynth.js
+++ 01_simplesynth/simplesynth.js
@@ -1,37 +1,167 @@
-import {createToggle} from '../common/browsersynth.js'
+import {createToggle, createVerticalSlider} from '../common/browsersynth.js'
 
 export const simpleSynth = () => {
     let ctx = null;
-    const oscTypes = ['sine','sawtooth','square'];
-    let oscType = oscTypes[0];
 
-    let ADSR = {
+    // oscilattors with decorators
+    const oscTypes = {
+        'sawtooth': { name: 'sawtooth',
+            iconDraw: (c) => {
+                const canvas = c.querySelector('.toggleButtonIcon');
+                const context = canvas.getContext('2d');
+                context.beginPath();
+                context.moveTo(20, 20);
+                context.lineTo(20, 40);
+                context.lineTo(40, 40);
+                context.closePath();
+                context.lineWidth = 2;
+                context.strokeStyle = '#666666';
+                context.stroke();
+                context.fillStyle = '#cf6680';
+                context.fill();
+            }
+        },
+        'square': { name: 'square',
+            iconDraw: (c) => {
+                const canvas = c.querySelector('.toggleButtonIcon');
+                const context = canvas.getContext('2d');
+                context.beginPath();
+                context.moveTo(20, 20);
+                context.lineTo(20, 40);
+                context.lineTo(40, 40);
+                context.lineTo(40, 20);
+                context.closePath();
+                context.lineWidth = 2;
+                context.strokeStyle = '#666666';
+                context.stroke();
+                context.fillStyle = '#cf6680';
+                context.fill();
+            }
+        },
+        'sine': { name: 'sine',
+            iconDraw: (c) => {
+                const canvas = c.querySelector('.toggleButtonIcon');
+                const context = canvas.getContext('2d');
+                context.beginPath();
+                context.moveTo(20, 40);
+                context.bezierCurveTo(20, 20, 40, 20, 40, 40);
+                context.stroke();
+                context.closePath();
+                context.lineWidth = 2;
+                context.strokeStyle = '#666666';
+                context.stroke();
+                context.fillStyle = '#cf6680';
+                context.fill();
+            }
+        },
+    };
+    let oscType = oscTypes.sawtooth;
+
+    const ADSR = {
         A: 0.1,
         D: 2.0,
         S: 100.0,
         R: 5.0,
-    }
+    };
+
+    const Filter = {
+        Cutoff: 10000,
+    };
+
+    const setStyles = (el, styles) => {
+        for (const k in styles) {
+            el.style[k] = styles[k];
+        }
+    };
 
     // build gui
     const gui = document.createElement('div');
     gui.classList.add('synth', 'simpleSynth');
 
+    // create cool logo
+    const logo = document.createElement('div');
+    logo.innerText = 'SimpleSynth';
+    logo.style.fontSize = '5rem';
+    logo.style.fontFamily = 'system, helvetical, arial, sans-serif';
+    logo.style.color = '#cf6680';
+    logo.style.padding = '2rem 0';
+    logo.style.textShadow = '0 -1px #666';
+    logo.style.textAlign = 'center';
+    gui.appendChild(logo);
+
+    // set up gui for controls
+    const mainControls = document.createElement('div');
+    mainControls.innerText = '';
+    setStyles(mainControls, {
+        alignItems: 'center',
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr',
+        gridGap: '1rem',
+        justifyItems: 'center',
+        padding: '2rem'
+    });
+
+    // oscType toggler
     const toggle = document.createElement('div');
-    toggle.innerHTML = createToggle({items: oscTypes, active: oscType}).outerHTML;
+    const initT = createToggle({items: oscTypes, active: oscType.name, iconDraw: oscType.iconDraw });
+    toggle.appendChild(initT);
+    if (oscType.iconDraw) {
+        oscType.iconDraw(initT);
+    }
     toggle.onclick = (e) => {
-        switch (oscType) {
+        let icon;
+        switch (oscType.name) {
             case 'sine':
-                oscType = 'sawtooth';
+                oscType = oscTypes.sawtooth;
                 break;
             case 'sawtooth':
-                oscType = 'square';
+                oscType = oscTypes.square;
                 break;
             default:
-                oscType = 'sine';
+                oscType = oscTypes.sine;
         }
-        toggle.innerHTML = createToggle({items: oscTypes, active: oscType}).outerHTML;
+        while (toggle.firstChild) {
+            toggle.removeChild(toggle.lastChild);
+        }
+        const r = createToggle({items: oscTypes, active: oscType.name, iconDraw: oscType.iconDraw });
+        toggle.appendChild(r);
+        if (oscType.iconDraw) {
+            oscType.iconDraw(r);
+        }
     };
-    gui.appendChild(toggle);
+    mainControls.appendChild(toggle);
+    gui.appendChild(mainControls);
+
+    // envelope
+    const env = document.createElement('div');
+    setStyles(env, {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(4,1fr)'
+    });
+    const aSlider = createVerticalSlider({label: 'Att', value: ADSR.A, onChange: (t) => {
+        ADSR.A = t.target.value;
+    }});
+    const dSlider = createVerticalSlider({label: 'Dec', value: ADSR.D, onChange: (t) => {
+            ADSR.D = t.target.value;
+        }});
+    const sSlider = createVerticalSlider({label: 'Sus', value: ADSR.S, onChange: (t) => {
+            ADSR.S = t.target.value;
+        }});
+    const rSlider = createVerticalSlider({label: 'Rel', value: ADSR.R, onChange: (t) => {
+            ADSR.R = t.target.value;
+    }});
+    env.appendChild(aSlider);
+    env.appendChild(dSlider);
+    env.appendChild(sSlider);
+    env.appendChild(rSlider);
+    mainControls.appendChild(env);
+
+    const filterControl = document.createElement('div');
+    const filtSlider = createVerticalSlider({label: 'Cutoff', value: 10000, max: 10000, onChange: (t) => {
+            Filter.Cutoff = t.target.value;
+        }});
+    filterControl.appendChild(filtSlider);
+    mainControls.appendChild(filterControl);
 
     return {
         gui,
@@ -41,17 +171,31 @@ export const simpleSynth = () => {
                 ctx = new (window.AudioContext || window.webkitAudioContext)();
             }
             const oscillator = ctx.createOscillator();
-            oscillator.type = oscType;
+            oscillator.type = oscType.name;
             oscillator.frequency.setValueAtTime(freq, ctx.currentTime);
 
             const gainNode = ctx.createGain();
-            gainNode.gain.value = .25;
-            oscillator.connect(gainNode);
-            gainNode.connect(ctx.destination);
+            gainNode.gain.value = .15;
 
+            //gainNode.connect(ctx.destination);
+
+            const filter = ctx.createBiquadFilter();
+            filter.frequency.value = Filter.Cutoff;
+            filter.type = 'lowpass';
+            oscillator.connect(filter);
+            //oscillator.connect(gainNode);
+
+            filter.connect(gainNode);
+            gainNode.connect(ctx.destination);
             oscillator.start();
+
+            if (ADSR.A > 0) {
+                gainNode.gain.value = 0;
+                gainNode.gain.linearRampToValueAtTime(0.15, ctx.currentTime + (maxLength * ADSR.A / 100.0) );
+            }
             gainNode.gain.linearRampToValueAtTime(0, ctx.currentTime + (maxLength * ADSR.R / 100.0) );
             oscillator.stop(ctx.currentTime + maxLength);
+
         },
     };
 };
--- common/browsersynth.js
+++ common/browsersynth.js
@@ -1,25 +1,38 @@
 export const browsersynth = () => {
 }
 
-export const createToggle = ({items = [], active = ''}) => {
+export const createToggle = ({items = [], active = '', iconDraw = null}) => {
     const t = document.createElement('div');
     t.classList.add('toggle');
 
     const button = document.createElement('div');
     button.classList.add('toggleButton');
+    const iconEl = document.createElement('canvas');
+    iconEl.setAttribute('width', '60px');
+    iconEl.setAttribute('height', '60px');
+    iconEl.style.position = 'absolute';
+    iconEl.style.left = '5px';
+    iconEl.style.top = '5px';
+    iconEl.classList.add('toggleButtonIcon');
+
+    button.appendChild(iconEl);
     t.appendChild(button);
 
     const options = document.createElement('div');
-    items.map(i => {
+    let arr = [];
+    for (const k in items) {
+        arr.push(items[k]);
+    }
+    arr.map((v,i) => {
        const el = document.createElement('div');
        el.classList.add('toggleOption')
        const indicator = document.createElement('div');
        indicator.innerHTML = '<span></span>';
-       if (i === active) {
+       if (v.name === active) {
            indicator.classList.add('active');
        }
        const label = document.createElement('div');
-       label.innerText = i;
+       label.innerText = v.name;
        el.appendChild(indicator);
        el.appendChild(label);
        options.appendChild(el);
@@ -27,4 +40,34 @@ export const createToggle = ({items = [], active = ''}) => {
     t.appendChild(options);
 
     return t;
+};
+
+
+export const createVerticalSlider = ({ label = '', value = 0, max = 100, onChange }) => {
+    const cont = document.createElement('div');
+    cont.style.display = 'grid';
+    cont.style.gridTemplateRows = '5fr 1fr';
+    cont.style.textAlign = 'center';
+
+    const slCont = document.createElement('div');
+    slCont.style.display = 'relative';
+
+    const sl = document.createElement('input');
+    sl.setAttribute('orient', 'vertical');
+    sl.setAttribute('type','range');
+    sl.setAttribute('min','1');
+    sl.setAttribute('max',max);
+    sl.setAttribute('value',value);
+    sl.classList.add('slider','vertical');
+    if (onChange) {
+        sl.onchange = onChange;
+    }
+    slCont.appendChild(sl);
+
+    const lb = document.createElement('div');
+    lb.innerText = label;
+
+    cont.appendChild(slCont);
+    cont.appendChild(lb);
+    return cont;
 };
